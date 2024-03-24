08a5f483b143068bc2f3a7130c2a4612bc499777
tran van dinh <tranvandinh0298@gmail.com>
2024-03-24 22:19:48 +0700

    init spring

---
+++ src/main/java/com/source/dinhtv/fashionecommercecore/FashionEcommerceCoreApplication.java
@@ -0,0 +1,13 @@
+package com.source.dinhtv.fashionecommercecore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FashionEcommerceCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FashionEcommerceCoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/source/dinhtv/fashionecommercecore/FashionEcommerceCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.source.dinhtv.fashionecommercecore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FashionEcommerceCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
